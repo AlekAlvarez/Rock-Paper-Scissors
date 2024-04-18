07df0315a7eabe1d59ae739df4207f4540bd94f3
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-18 16:34:49 -0500

    Adds round function

--- script.js
+++ script.js
@@ -9,3 +9,34 @@ function getComputerChoice(){
             return "Scissors";
     }
 }
+function playRound(playerChoice, computerChoice){
+    playerChoice=playerChoice.toLowerCase();
+    computerChoice=computerChoice.toLowerCase();
+    if(playerChoice==computerChoice){
+        return "You tie! You both choose "+playerChoice;
+    }
+    if(playerChoice=="rock"){
+        if(computerChoice=="paper"){
+            return "You loose! The Computers Paper beats your Paper";
+        }
+        else{
+            return "You Win! Your Rock beats the Computer's Scissors";
+        }
+    }
+    else if(playerChoice=="paper"){
+        if(computerChoice=="rock"){
+            return "You Win! Your Paper beats the Computers Rock";
+        }
+        else{
+            return "You lose! The Computers Scissors beat your Rock";
+        }
+    }
+    else{
+        if(computerChoice=="rock"){
+            return "You loose! The Computers Rock beat your Scissors";
+        }
+        else{
+            return "You Win! Your Scissors beat the Computers paper";
+        }
+    }
+}
