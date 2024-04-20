2e458de1ff1d76e28708c9140ad97eaad28475bb
Aleksander Alvarez <alekalvarez123@gmail.com>
2024-04-19 22:06:27 -0500

    redines README for this branch and integrates GUI into Game

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 This Site shall use HTML and Javascript to create a responsive site
 where the user can play Tic-Tac-Toe and hopefully will implement a 
 GUI after the intital console based application.
+This branch is a temporary one to implement GUI functionality.
--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let win=0;let loss=0;
+const round=document.querySelector("#round");
 function getComputerChoice(){
     let choice=Math.floor(Math.random()*3);
     switch(choice){
@@ -13,67 +15,86 @@ function playRound(playerChoice, computerChoice){
     playerChoice=playerChoice.toLowerCase();
     computerChoice=computerChoice.toLowerCase();
     if(playerChoice==computerChoice){
-        console.log("You tie! You both choose "+playerChoice);
+        round.innerText="You tie! You both choose "+playerChoice;
         return 0;
     }
     if(playerChoice=="rock"){
         if(computerChoice=="paper"){
-            console.log("You loose! The Computers Paper beats your Rock");
+            round.innerText="You loose! The Computers Paper beats your Rock";
             return -1;
         }
         else{
-           console.log("You Win! Your Rock beats the Computer's Scissors");
+           round.innerText="You Win! Your Rock beats the Computer's Scissors";
            return 1;
         }
     }
     else if(playerChoice=="paper"){
         if(computerChoice=="rock"){
-            console.log("You Win! Your Paper beats the Computers Rock");
+            round.innerText="You Win! Your Paper beats the Computers Rock";
             return 1;
         }
         else{
-            console.log("You lose! The Computers Scissors beat your Paper");
+            round.innerText="You lose! The Computers Scissors beat your Paper";
             return -1;
         }
     }
     else{
         if(computerChoice=="rock"){
-            console.log("You loose! The Computers Rock beat your Scissors");
+            round.innerText="You loose! The Computers Rock beat your Scissors";
             return -1;
         }
         else{
-            console.log("You Win! Your Scissors beat the Computers paper");
+            round.innerText="You Win! Your Scissors beat the Computers paper";
             return 1;
         }
     }
 }
-function playGame(){
-    let win=0,lose=0;
-    for(let i=0;i<5;i++){
-        console.log("You:"+win+" Computer:"+lose);
-        let playerChoice=window.prompt("What do you choose?");
-        let result=playRound(playerChoice,getComputerChoice());
-        if(result==1){
-            win++;
-        }
-        else if(result==-1){
-            lose++;
-        }
-        else{
-            win++;
-            lose++;
-        }
-    }
-    console.log("Final Score");
-    console.log(`You: ${win} Computer: ${lose}`);
-    if(win>lose){
-        console.log("Overrall You Win");
-    }
-    else if(win<lose){
-        console.log("Overrall You lose");
+function updateScore(){
+    const div=document.querySelector("#results");
+    if(win<5&&loss<5){
+    div.innerText=`You:${win} Computer${loss}`;
     }
     else{
-        console.log("Overral You Tie");
+        if(win>=5){
+            div.innerText="You Win";
+        }
+        else{
+            div.innerText="All hail the machines you lose";
+        }
     }
+
 }
-playGame();
+document.addEventListener("load",updateScore());
+const rock=document.querySelector("#R");
+const paper=document.querySelector("#P");
+const scissors=document.querySelector("#S");
+rock.addEventListener("click",()=>{
+    let score=playRound("rock",getComputerChoice());
+    if(score>0){
+        win++;
+    }  
+    if(score<0){
+        loss++;
+    }
+    updateScore();
+});
+paper.addEventListener("click",()=>{
+    let score=playRound("paper",getComputerChoice());
+    if(score>0){
+        win++;
+    }  
+    if(score<0){
+        loss++;
+    }
+    updateScore();
+});
+scissors.addEventListener("click",()=>{
+    let score=playRound("scissors",getComputerChoice());
+    if(score>0){
+        win++;
+    }  
+    if(score<0){
+        loss++;
+    }
+    updateScore();
+});
